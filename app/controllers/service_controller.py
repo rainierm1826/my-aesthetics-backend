ccb0f41056ffa1138ce9adcc78788d6c29e17e8c
rainierm1826 <76774565+rainierm1826@users.noreply.github.com>
2025-09-06 10:20:39 +0800

    fix

--- app/controllers/service_controller.py
+++ app/controllers/service_controller.py
@@ -13,7 +13,7 @@ class ServiceController(BaseCRUDController):
             required_fields=["service_name", "price", "is_sale", "category", "image"],
             searchable_fields=["service_name"],
             filterable_fields={"category": "category", "branch": (Branch, "branch_id")},
-            updatable_fields=["service_name", "branch_id", "original_price", "is_sale", "discount_percentage", "category", "image"],
+            updatable_fields=["service_name", "branch_id", "original_price", "is_sale", "discount_percentage", "category", "image", "description"],
             sortable_fields={"price": Service.price, "service": Service.service_name, "rate": Service.average_rate},
             joins=[(Branch, Service.branch_id == Branch.branch_id, "left")]
         )
