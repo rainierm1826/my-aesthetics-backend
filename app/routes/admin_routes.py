abf66988db07b711e505b01d80f34912d28229bc
rainierm1826 <76774565+rainierm1826@users.noreply.github.com>
2025-09-21 10:03:24 +0800

    rbac implemented

--- app/routes/admin_routes.py
+++ app/routes/admin_routes.py
@@ -8,19 +8,14 @@ admin_controller = AdminController()
 
 
 @admin_bp.route("/all", methods=["GET"])
-# @jwt_required()
-# @access_control("owner")
+@jwt_required()
+@access_control("owner")
 def get_all_admin():
     return admin_controller.get_all()
 
-@admin_bp.route("/", methods=["GET"])
-# @jwt_required()
-# @access_control("admin")
-def get_admin():
-    return admin_controller.get_by_id()
 
 @admin_bp.route("", methods=["PATCH"])
-# @jwt_required()
-# @access_control("admin", "owner")
+@jwt_required()
+@access_control("owner")
 def update_admin():
     return admin_controller.owner_update_admin()
